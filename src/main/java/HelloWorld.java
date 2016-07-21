7c0a7a26717ec52eaa71f708f1df895482b84d87
Sergiu <sergiu.lazar7243@yahoo.com>
2016-07-21 10:05:05 +0300

    jenkins commit

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,15 @@
+/**
+ * Created by slazaran on 7/15/2016.
+ */
+public class HelloWorld {
+
+    public static String CreateMessage(int x)
+    {
+        if (x == 1) {
+            return "Hello World!";
+        } else {
+            return "Goodbye world";
+        }
+
+    }
+}
