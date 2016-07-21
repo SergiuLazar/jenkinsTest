07729bf96d8cd26f5dd1dde3a6840041316ccd19
Sergiu <sergiu.lazar7243@yahoo.com>
2016-07-21 11:26:53 +0300

    jenkins commit

---
+++ src/test/java/UnitTest.java
@@ -0,0 +1,20 @@
+import junit.framework.TestCase;
+import org.junit.Test;
+import static  org.junit.Assert.*;
+
+/**
+ * Created by slazaran on 7/15/2016.
+ */
+
+public class UnitTest {
+
+    @Test
+    public void testMethodPass() {
+        assertTrue(HelloWorld.CreateMessage(0).equals("Goodbye world"));
+    }
+
+    @Test
+    public void testMethodFail() {
+        assertTrue(HelloWorld.CreateMessage(1).equals("Hello World!"));
+    }
+}
